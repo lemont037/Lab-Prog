0343e0e1df0c3a0cafb304d07247ffc38ec03b09
Your Name <you@example.com>
2019-04-04 09:42:02 -0300

    o erro persiste

--- Trab1/Main.cpp
+++ Trab1/Main.cpp
@@ -20,9 +20,9 @@ T* gerar_instancia(int n){
 //swap basico
 template <typename T>
 void swap(T *a, T *b){
-    T aux = *a;
-    *a = *b;
-    *b = aux;
+	T aux = *a;
+	*a = *b;
+	*b = aux;
 }
 
 int escolher_pivo (int primeiro, int ultimo){
@@ -34,25 +34,48 @@ int escolher_pivo (int primeiro, int ultimo){
 template <typename T>
 T *particiona(T *inicio, T *fim, T *pivo){
     // swap(inicio, pivo);
-    T *limite = inicio+1;
-    for (T *i = inicio+1; i != fim; ++i){
-        if(*i <= *inicio){
-            swap(i, limite);
-            limite++;           
-        }
-    }
-    swap(inicio, limite-1);
-    return limite-1;
+	T *limite = inicio+1;
+	for (T *i = inicio+1; i != fim; ++i){
+		if(*i <= *inicio){
+			swap(i, limite);
+			limite++;           
+		}
+	}
+	swap(inicio, limite-1);
+	return limite-1;
 }
 
 template <typename T>
 void quicksort_indices(T* vetor, int inicio, int fim){
-	int pivo = escolher_pivo(inicio, fim);
-	T aux = vetor[inicio];
-	vetor[inicio] = vetor[pivo];
-	vetor[pivo] = aux;
-	for (int i = inicio; i < fim-inicio; ++i){
-		
+	T aux;
+	if(fim-inicio>1){
+		int pivo = escolher_pivo(inicio, fim);
+		aux = vetor[inicio];
+		vetor[inicio] = vetor[pivo];
+		vetor[pivo] = aux;
+		int limite = inicio+1;
+		for (int i = inicio+1; i <= fim; ++i){
+			if (vetor[i] < vetor[inicio]){
+				// if(i != limite){
+					aux = vetor[limite];
+					vetor[limite] = vetor[i];
+					vetor[i] = aux;
+					limite++;
+				// }
+			}
+		}
+		aux = vetor[limite];
+		vetor[limite] = vetor[inicio];
+		vetor[inicio] = aux;
+
+		quicksort_indices(vetor, inicio,(fim+inicio)/2);
+		quicksort_indices(vetor, (fim+inicio)/2, fim);
+	}else if (fim-inicio==1){
+		if (vetor[inicio] > vetor[fim]){
+			aux = vetor[inicio];
+			vetor[inicio] = vetor[fim];
+			vetor[fim] = aux;
+		}
 	}
 }
 
@@ -60,10 +83,21 @@ int main(int argc, char const *argv[]){
 	// gerar_instancia<int> gerar_instancia_int;
 	int n;
 	cin >> n; 
+	cout << endl;
 	int* ac = gerar_instancia<int>(n);
+	int ac[] = {4,2,3,2};
+
+	// if(!(escrever_instancia<int>(ac, n))){
+		for (int i = 0; i < n; ++i){
+			cout << ac[i] << endl;
+		}
+
+	// }
+	cout << endl;
+	quicksort_indices(ac, 0, n-1);
+	
 	for (int i = 0; i < n; ++i){
 		cout << ac[i] << endl;
-	}
-
+	}	
 	return 0;
 }
