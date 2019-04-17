d42705f32d6f7f97cf761cc313e8702ffa35a5f5
Your Name <you@example.com>
2019-04-17 06:22:18 -0300

    recurssao de cauda pronto

--- Trab1/Main.cpp
+++ Trab1/Main.cpp
@@ -1,10 +1,10 @@
+
 #include "instancias_ruins_Quicksort.hpp"
 #include <random>
 #include <iostream>
 #include <new>
 using std::nothrow;
 using std::cout;using std::endl;using std::cin;
-
 
 // Gerar vetor com n termos aleatorios de 1 a n
 template <typename T>
@@ -54,6 +54,22 @@ T *particiona(T *inicio, T *fim, T *pivo){
 	return limite-1;
 }
 
+// Particionamento triplo
+template <typename T>
+T* particiona_tri(T *inicio, T *fim, T *pivo){
+	swap(inicio, pivo);
+	T *limite_1 = inicio+1;
+	T retorno[2];
+	for (T *i = inicio+1; i != fim; ++i){
+		if(*i < *inicio){
+			swap(i, limite_1);
+			limite_1++;           
+		}
+	}
+	swap(inicio, limite_1-1);
+	return retorno;
+}
+
 // Particiona usando indices
 template <typename T>
 int particiona_i(T *vetor, int inicio, int fim, int pivo){
@@ -90,6 +106,27 @@ void quicksort_i(T* vetor, int inicio, int fim){
 		quicksort_i(vetor, pivo+1, fim);
 	}
 }
+
+
+template <typename T>
+void  quicksort_cauda(T* vetor, int inicio, int fim){
+	if(inicio < fim){
+
+		int pivo = escolher_pivo(inicio, fim);
+		T*  p_pivo = particiona(vetor+inicio,  vetor+fim, vetor+pivo);
+		pivo = p_pivo-vetor;
+		quicksort_cauda(vetor, inicio, pivo);
+		
+		while(pivo+1 < fim){
+			inicio = pivo+1;
+			pivo = escolher_pivo(inicio, fim);
+			p_pivo = particiona(vetor+inicio,  vetor+fim, vetor+pivo);
+			pivo = p_pivo-vetor;
+			quicksort_cauda(vetor, inicio, pivo);
+		}
+
+	}
+}
 void quicksort_teste(){
 	// gerar_instancia<int> gerar_instancia_int;
 	int n;
@@ -99,6 +136,7 @@ void quicksort_teste(){
 	// int* instancia_1;
 	int* instancia_1 = gerar_instancia<int>(n);
 	int instancia_2[n];
+	int instancia_3[n];
 
 	// for (int i = 0; i < n; ++i){
 	// 	instancia_2[i] = instancia_1[i];
@@ -109,32 +147,43 @@ void quicksort_teste(){
 	// n= 5;
 
 	// if(!(escrever_instancia<int>(instancia_1, n))){
-	cout <<"q1\tq2"<<endl;
+	// cout <<"q1\tq2\tq3"<<endl;
 	for (int i = 0; i < n; ++i){
 		instancia_2[i] = instancia_1[i];
-		cout << instancia_1[i]<<"\t"<<instancia_2[i] << endl;
+		instancia_3[i] = instancia_1[i];
+		if(n<=100)
+			cout << instancia_1[i]<<"\t"<<instancia_2[i]<<"\t"<<instancia_3[i] << endl;
 	}
 
 	// }
 	cout << endl;
 	cout << endl;
 
-	quicksort_p(instancia_1, 0, n);
-	quicksort_i(instancia_2, 0, n);
-	// particiona(instancia_1, instancia_1+n, instancia_1);
-	// particiona_i(instancia_2, 0, n, 0);
-
+	clock_t tempo[4];
+	tempo[0] = clock();
+	quicksort_i(instancia_1, 0, n);
+	tempo[1] = clock()-tempo[0];	
+	quicksort_p(instancia_2, 0, n);
+	tempo[2] = clock()-tempo[1];	
+	quicksort_cauda(instancia_3, 0, n);
+	tempo[3] = clock()-tempo[2];	
 	
-	cout <<"q1\tq2"<<endl;
-	for (int i = 0; i < n; ++i){
-		cout << instancia_1[i]<<"\t"<<instancia_2[i] << endl;
-		
-	}
+	
+	cout <<"q1\tq2\tq3"<<endl;
+	if(n<=100)
+		for (int i = 0; i < n; ++i)
+			cout << instancia_1[i]<<"\t"<<instancia_2[i]<<"\t"<<instancia_3[i] << endl;	
+
+	for (int i = 1; i < 4; ++i)
+		cout << ( (long double)(tempo[i]) ) <<"\t";
+	cout << endl;
+
+	for (int i = 0; i < 4; ++i)
+		cout << (double)tempo[i]/CLOCKS_PER_SEC << endl;
 }
 
 
 int main(int argc, char const *argv[]){
-	quicksort_teste()	;
-
+	quicksort_teste();
 	return 0;
 }
